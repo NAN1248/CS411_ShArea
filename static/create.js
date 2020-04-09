38d3163bf46de8c33cf2f8a702d12ebd3cd64f27
archisha.m <architen100@gmail.com>
2020-04-08 21:45:44 -0700

    added create.js

---
+++ static/create.js
@@ -0,0 +1,25 @@
+var doCreate = function() {
+    const email = $('#email').val()
+    const password = $('#password').val()
+    const data = {
+        "email": email,
+        "password": password
+    }
+    fetch("http://127.0.0.1:5000/create_user", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
+    }).then(response => {
+        response.json().then(data => {
+            const name = data.email
+            $("#showName").val(name);
+        })
+    });
+}
+
+
+$("input#createButton").click(function() {
+        doCreate();
+});
