4cee5d7577a9608d07ec3a625a2d3d3e33f746c5
wennan-er <54024010+wennan-er@users.noreply.github.com>
2020-05-03 04:10:33 +0800

    removed all my data for test

--- events.py
+++ events.py
@@ -39,9 +39,8 @@ class Eventtag(Resource):
     def get(self, _tag):
         events = mongo.db.events
         output = []
-        # make everything lowercase
-        _tag = _tag.lower()
-        print(_tag)
+        # capitalize 
+        _tag = _tag[:1].upper() + _tag[1:]
         for e in events.find({'tags' : _tag}):
             output.append({'id' : e['id'], 'start_time' : e['start_time'], 'duration':e['duration'], 'tags':e['tags']})
         return jsonify({'result' : output})
